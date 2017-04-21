78aa8e3e80cf0ab7264929528668ea8765309664
antonioaren <antonioaren@hotmail.com>
2017-04-21 02:44:10 +0100

    Added LoadItems without progressBar. Doubt how to connect presenter to model not using presenter call from model. Always calling presenter to model and receive an Arraylist.

--- StudyDA/src/main/java/es/ulpgc/eite/clean/mvp/sample/main/MainModel.java
+++ StudyDA/src/main/java/es/ulpgc/eite/clean/mvp/sample/main/MainModel.java
@@ -3,9 +3,11 @@ package es.ulpgc.eite.clean.mvp.sample.main;
 import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import es.ulpgc.eite.clean.mvp.GenericModel;
 
+import es.ulpgc.eite.clean.mvp.sample.modelview.ModelItem;
 import es.ulpgc.eite.clean.mvp.sample.storage.Storage;
 
 
@@ -18,6 +20,8 @@ public class MainModel extends GenericModel<Main.ModelToPresenter>
     private Integer l;
 
     private String ButtonNextPageLabel;
+
+    public List<ModelItem> items = null;
 
 
 
@@ -57,6 +61,8 @@ public class MainModel extends GenericModel<Main.ModelToPresenter>
     public String getName() {
         return Storage.getInstance().getNames(getK());
     }
+
+
     ////////////////////////////////////////////////////////////////////////////////////////////////
     //////////////////////////////////// Puntero Dinámico //////////////////////////////////////////
 
@@ -65,10 +71,7 @@ public class MainModel extends GenericModel<Main.ModelToPresenter>
         k = 0;
     }
 
-    @Override
-    public void LoadItems() {
 
-    }
 
     @Override
     public Integer getK() {
@@ -135,5 +138,30 @@ public class MainModel extends GenericModel<Main.ModelToPresenter>
     @Override
     public String getButtonNextPageLabel() {
         return ButtonNextPageLabel;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    //////////////////////////////////////// Master ////////////////////////////////////////////////
+
+    @Override
+    public void LoadItems() {
+        items = new ArrayList();
+        for (int count = 0; count <= 9; count++) {
+            items.add(new ModelItem(String.valueOf(count), "Item " + count, makeDetails(count)));
+        }
+    }
+
+    private String makeDetails(int position) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Details about Item: ").append(position).append("\n");
+
+        for (int count = 0; count < position; count++) {
+            builder.append("\nMore details information here.");
+        }
+        return builder.toString();
     }
 }
